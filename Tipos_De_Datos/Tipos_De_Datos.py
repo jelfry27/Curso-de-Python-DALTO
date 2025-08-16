78f12411ee1b5a4d7c23bd9b3cbf6360284857b3
JELFRY <jelfry2701@gmail.com>
2025-08-15 21:19:28 -0400

    Desde_Casa

---
+++ Tipos_De_Datos/Tipos_De_Datos.py
@@ -0,0 +1,22 @@
+text = "String"
+
+def variable(saludo):
+    print(saludo)
+
+variable("""
+Mis Datos Son:
+    Nombre: JELFRY
+    Apellido: NUNEZ
+""")
+
+Int_Numero = 40
+Float_Decimal = 40.2
+Complex_Ingeniero = 20j
+
+print ((Int_Numero))
+print ((Float_Decimal))
+print ((Complex_Ingeniero))
+
+# True_And_False_Son_Booleanos
+True
+False
---
+++ Tipos_De_Datos/Variable/Variables.py
@@ -0,0 +1,27 @@
+a = 2
+b = 5
+c = a * b * a + 1
+
+print(c)
+# La variable es cascada
+def nombre(Programador):
+    print(Programador)
+    
+nombre("JELFRY")
+
+Saludo = "Hola"
+nombreA = "JELFRY_A"
+print(Saludo + ", Como Estas " + nombreA)
+
+def Fin_De_Semana(Practica):
+    print(Practica)
+ 
+Grupo = "Amigos"
+Fiesta = "Diversion" #
+Viernes = "Salida Sin " + Grupo + " Es " + Fiesta
+Sabado = "Nuevo Dia"
+Dias = 6
+Fecha = "Sabado"
+Domigo = f"Hoy es Domigo. En {Dias} Dias Es {Fecha}"
+Fin_De_Semana(Viernes + ". Hoy Es Un " + Sabado)
+print(Domigo)
